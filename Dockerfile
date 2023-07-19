eb56c5ea100215e6d962e0d2fe925f07f02294ee
breakdowns <71178188+breakdowns@users.noreply.github.com>
2023-07-19 19:48:26 +0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+FROM phusion/baseimage:bionic-1.0.0
+
+# Use baseimage-docker's init system:
+CMD ["/sbin/my_init"]
+
+# Install dependencies:
+RUN apt-get update && apt-get install -y \
+    bash \
+    curl \
+    sudo \
+    wget \
+    git \
+    make \
+    busybox \
+    build-essential \
+    nodejs \
+    npm \
+    ffmpeg \
+    unzip \
+    python3-pip \
+    python3 \
+    python3-dev \
+ && mkdir -p /home/stuff
+# Set work dir:
+WORKDIR /home
+
+# Copy files:
+COPY startbot.sh /home
+COPY config.sh /home
+COPY /stuff /home/stuff
+
+# Run config.sh and clean up APT:
+RUN sh /home/config.sh \
+ && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Install the bot:
+RUN git clone https://github.com/botgram/shell-bot.git \
+ && cd shell-bot \
+ && npm install
+
+RUN echo "Uploaded files:" && ls /home/stuff/
+
+# Run bot script:
+CMD bash /home/startbot.sh
---
+++ config.sh
@@ -0,0 +1,34 @@
+#!bin/sh
+
+# put your commands to install extra shit and configure it below:
+# Check your logs on heroku to see if you were sucessfull
+# default dir is /home, the stuff folder's dir is /home/stuff/
+
+echo "NOTICE: config.sh is runnning"
+
+### EXAMPLE - Gclone:
+
+## To install gclone:
+
+# apt-get install -y systemd golang unzip
+# mkdir -p /home/.config/rclone/
+# curl -s https://raw.githubusercontent.com/oneindex/script/master/gclone.sh | sudo bash
+
+## To configure gclone paste your config in between both "EOF", eg:
+
+# echo > /home/.config/rclone/rclone.conf << EOF
+# [blah]
+# dskjkghs
+# skfg=jsdhgjk
+# longasstoken=shdgskdhgdsg:4564657657215412945^#%^*^$*
+# EOF
+
+# Requirements
+pip3 install requests
+pip3 install bs4
+pip3 install cython
+pip3 install git+https://github.com/TheBreakdowns/lk21
+
+# Repo
+git clone https://github.com/DARK-02/InstaFollower.git
+git clone https://github.com/Iyee-Code/Crack-Ig.git
